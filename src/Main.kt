602d29ae501c0549cbaa09ba7f753b7fdb897118
hywenklis <hywenklis@hotmail.com>
2021-10-30 12:27:06 -0300

    Criando classe mais especifica funcionario do tipo gerente

--- src/Main.kt
+++ src/Main.kt
@@ -12,4 +12,27 @@ fun main() {
     println("cpf ${hywenklis.cpf}")
     println("salario ${hywenklis.salario}")
     println("Bonificação ${hywenklis.bonificacao()}")
+
+    println()
+
+    println("Bem vindo ao ByteBank")
+
+    val maria = Gerente(
+        nome = "Maria",
+        cpf = "111.111.111-11",
+        salario = 3000.0,
+        senha = 123
+    )
+
+    println("nome ${maria.nome}")
+    println("cpf ${maria.cpf}")
+    println("salario ${maria.salario}")
+    println("Bonificação ${maria.bonificacao()}")
+
+    if (maria.autentica(123)) {
+        println("Autenticado!")
+    } else {
+        println("Não autenticado!")
+    }
+
 }
