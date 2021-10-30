4dccb6c7d02115196d43d564a317cd89ccf7b8e0
hywenklis <hywenklis@hotmail.com>
2021-10-30 12:02:40 -0300

    Criando classe funcionario

--- src/Main.kt
+++ src/Main.kt
@@ -1,10 +1,15 @@
 fun main() {
 
     println("Bem vindo ao ByteBank")
-    testaComportamentoConta()
+
+    val hywenklis = Funcionario(
+        nome = "hywenklis",
+        cpf = "111.111.111-11",
+        salario = 1000.0
+    )
+
+    println("nome ${hywenklis.nome}")
+    println("cpf ${hywenklis.cpf}")
+    println("salario ${hywenklis.salario}")
+    println("Bonificação ${hywenklis.bonificacao()}")
 }
-
-
-
-
-
